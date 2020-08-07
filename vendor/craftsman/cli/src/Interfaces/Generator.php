3805fc0ecd1b1c8bdef558e011f34beca49db2b5
Kununya Sriwong <kununya.s@23perspective.com>
2020-08-07 16:21:00 +0700

    accesstrade api

--- vendor/craftsman/cli/src/Interfaces/Generator.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Craftsman\Interfaces;
-
-interface Generator{}
-
-
-/* End of file Generator.php */
-/* Location: ./src/Codeigniter/Interfaces/Generator.php */
--- vendor/craftsman/cli/src/Interfaces/Migration.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Craftsman\Interfaces;
-
-interface Migration{}
-
-
-/* End of file Migration.php */
-/* Location: ./src/Codeigniter/Interfaces/Migration.php */
