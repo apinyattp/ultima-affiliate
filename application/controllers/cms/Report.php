40d70f3c041df0c82b1510334cd7fca3c2e5234a
Kununya Sriwong <kununya.s@23perspective.com>
2020-09-03 19:06:31 +0700

    [doing] conversion report

--- application/controllers/cms/Report.php
+++ application/controllers/cms/Report.php
@@ -31,6 +31,8 @@ class Report extends MY_Controller {
             'conversion_id_desc' => 'conversion_id DESC',
             'conversion_time_asc' => 'conversion_time ASC',
             'conversion_time_desc' => 'conversion_time DESC',
+            'datetime_updated_asc' => 'datetime_updated ASC, id DESC',
+            'datetime_updated_desc' => 'datetime_updated desc, id DESC',
         ];
         if(!isset($a_sort[$sort])) $sort = 'datetime_updated_desc';
 
