b0af2283307c83a07ea22eb888576a6072e6deb5
Kununya Sriwong <kununya.s@23perspective.com>
2020-09-18 12:48:27 +0700

    [add][doing] admitad

--- application/views/cms/report/list/index.php
+++ application/views/cms/report/list/index.php
@@ -8,46 +8,48 @@
       </div>
       <?php $this->load->view('cms/report/list/search_filter'); ?>
       <div class="card-body">
-        <table class="table table-responsive">
-          <thead>
-            <tr class="text-nowrap">
-              <th scope="col">Conversion ID</th>
-              <!-- <th scope="col">Transaction ID</th> -->
-              <th scope="col">Campaign</th>
-              <th scope="col">UID</th>
-              <th scope="col">Cashback</th>
-              <th scope="col" class="text-center">Click Time</th>
-              <th scope="col">Conversion Time</th>
-              <th scope="col">Confirmation Time</th>
-              <th scope="col">Updated Time</th>
-              <th scope="col">Status</th>
-              <th class="text-center" scope="col">Detail</th>
-            </tr>
-          </thead>
-          <tbody class="font-size-08">
-            <?php foreach($a_conversion['lists'] as $conversion) { ?>
-              <tr>
-                <td><?php echo $conversion['conversion_id'] ?></td>
-                <!-- <td><?php echo $conversion['verification_id'] ?></td> -->
-                <td><?php echo $conversion['a_campaign']['name'] ?></td>
-                <td><?php echo $conversion['uid'] ?></td>
-                <td class="text-right"><?php echo $conversion['reward'] ?></td>
-                <td class="text-center"><?php echo $conversion['click_time'] ?></td>
-                <td class="text-center"><?php echo $conversion['conversion_time'] ?></td>
-                <td class="text-center"><?php echo $conversion['confirmation_time'] ?></td>
-                <td class="text-center"><?php echo $conversion['datetime_updated'] ?></td>
-                <td><?php echo $conversion['status'] ?></td>
-                <td class="text-center">
-                  <form action="<?php echo site_url('cms/report/detail/' . $conversion['id']); ?>">
-                    <button class="btn btn-info btn-sm">
-                      <i class="far fa-eye"></i>
-                    </button>
-                  </form>
-                </td>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr class="text-nowrap">
+                <th scope="col">Conversion ID</th>
+                <!-- <th scope="col">Transaction ID</th> -->
+                <th scope="col">Campaign</th>
+                <th scope="col">UID</th>
+                <th scope="col">Cashback</th>
+                <th scope="col" class="text-center">Click Time</th>
+                <th scope="col">Conversion Time</th>
+                <th scope="col">Confirmation Time</th>
+                <th scope="col">Updated Time</th>
+                <th scope="col">Status</th>
+                <th class="text-center" scope="col">Detail</th>
               </tr>
-            <?php } ?>
-          </tbody>
-        </table>
+            </thead>
+            <tbody class="font-size-08">
+              <?php foreach($a_conversion['lists'] as $conversion) { ?>
+                <tr>
+                  <td><?php echo $conversion['conversion_id'] ?></td>
+                  <!-- <td><?php echo $conversion['verification_id'] ?></td> -->
+                  <td><?php echo $conversion['a_campaign']['name'] ?></td>
+                  <td><?php echo $conversion['uid'] ?></td>
+                  <td class="text-right"><?php echo $conversion['reward'] ?></td>
+                  <td class="text-center"><?php echo $conversion['click_time'] ?></td>
+                  <td class="text-center"><?php echo $conversion['conversion_time'] ?></td>
+                  <td class="text-center"><?php echo $conversion['confirmation_time'] ?></td>
+                  <td class="text-center"><?php echo $conversion['datetime_updated'] ?></td>
+                  <td><?php echo $conversion['status'] ?></td>
+                  <td class="text-center">
+                    <form action="<?php echo site_url('cms/report/detail/' . $conversion['id']); ?>">
+                      <button class="btn btn-info btn-sm">
+                        <i class="far fa-eye"></i>
+                      </button>
+                    </form>
+                  </td>
+                </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
   
         <?php
           $a_conversion['pagination']['path'] = 'cms/report/list';
