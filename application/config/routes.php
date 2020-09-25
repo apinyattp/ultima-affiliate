f57be89128eef7921c8d5936c0dc8706628ca78a
Kununya Sriwong <kununya.s@23perspective.com>
2020-09-25 16:45:36 +0700

    [add][done] my404 redirect

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,5 +2,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['default_controller'] = 'api/example/welcome';
-$route['404_override'] = '';
+$route['404_override'] = 'my404';
 $route['translate_uri_dashes'] = FALSE;
