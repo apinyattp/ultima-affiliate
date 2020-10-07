df4247f849554022c1ee25f8c313a943c24ad40c
Kununya Sriwong <kununya.s@23perspective.com>
2020-10-07 14:00:04 +0700

    [debug] admitad report

--- application/controllers/crontab/Report.php
+++ application/controllers/crontab/Report.php
@@ -120,11 +120,10 @@ class Report extends MY_Controller {
                 $reward = $conversion['payment'] * $rate;
 
                 $transaction_amount = $conversion['cart'] * $rate;
-print_r($status);
-print_r($a_conversion);die();
+
                 // UPDATE STATUS
-                $a_conversion = $this->report_conversion_model->get_by_conversion_id($conversion_id);
-                if($status != 'PENDING' && !empty($a_conversion)) {
+                $conversion_data = $this->report_conversion_model->get_by_conversion_id($conversion_id);
+                if($status != 'PENDING' && !empty($conversion_data)) {
                     $this->report_conversion_model->update_status(
                         $conversion_id, 
                         $status, 
