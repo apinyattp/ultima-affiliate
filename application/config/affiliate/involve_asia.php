b8ea520265e99cc1a2d2ae1e1473ac8304d0d73e
Kununya Sriwong <kununya@harmonyx.co>
2020-12-24 13:47:13 +0700

    add involve asia offer

--- application/config/affiliate/involve_asia.php
+++ application/config/affiliate/involve_asia.php
@@ -8,8 +8,15 @@ $config = array(
     'a_offer_id' => [
         2806, // Shopee (TH)
         2523, // Lazada (TH)
-        760,
-        1985,
+        760, // Central (TH)
+        803, // Klook Travel
+        925, // Foreo
+        1179, // Nike (APAC)
+        1548, // King Power (TH)
+        1638, // AliExpress (Global)
+        1679, // Alibaba (CPS)
+        1980, // Power Buy
+        1985, // Adidas Thailand
         2107, // Sky scanner
         2204, // Tops (TH)
         2162, // Trip.com
@@ -17,5 +24,30 @@ $config = array(
         1243, // Sephora,
         2123, // Agoda Hotel
         2134, // Agoda Flight
+        2444, // GrabFood (TH) - CPA
+        1954, // Robinson
+        2644, // Lalamove Bangkok (TH)
+        2005, // konvy,
+        2024, // Razer (Global)
+        1564, // DJI CPS (Global)
+        2897, // NITAN (TH)  Shopeemall
+        2695, // Realme TH - Lazmall
+        2699, // Pomelo TH - Lazmall
+        2698, // OPPO Official Store TH - Lazmall,
+        2846, // My Home Design (TH) - Lazmall
+        2703, // Mi Flagship Store TH - Lazmall
+        2696, // Mamypoko TH - Lazmall,
+        2697, // Loreal Paris TH - Lazmall
+        2847, // UGREEN TH - Lazmall
+        2844, // TCL Official Store (TH) - Lazmall
+        2700, // Konvy TH - Lazmall
+        2845, // Hisense TH - Lazmall
+        2693, // Enfagrow TH - Lazmall
+        2843, // Converse Shop - Lazmall
+        2849, // Coca Cola TH - Lazmall
+        2848, // Cetaphil TH - Lazmall,
+        2842, // BIODERMA TH - LAZMALL,
+        2840, // Banfang (TH) Lazmall
+        2701, // ADDA TH - Lazmall
     ]
 );
