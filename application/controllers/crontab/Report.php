528e503ebc0fc14927d399aa34088ff8524f5541
Apinya <apinya@harmonyx.co>
2021-01-21 13:47:15 +0700

    [edit][working] debug

--- application/controllers/crontab/Report.php
+++ application/controllers/crontab/Report.php
@@ -188,10 +188,7 @@ class Report extends MY_Controller {
         
                 $campaign_code = $this->gen_campaign_code('IVA', $conversion['offer_id']);
                 $a_campaign = $this->campaign_model->get_by_code($campaign_code);
-                echo $this->db->last_query();
-                print_r( $a_conversion);
-                print_r( $campaign_code);
-                print_r($a_campaign);die();
+
                 $campaign_id = $a_campaign['id'];
                 $campaign_name = $conversion['offer_name'];
 
