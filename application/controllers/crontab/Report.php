f756a905a7a0f2b074c883b8dfa478d109f9e1ae
Apinya <apinya@harmonyx.co>
2021-01-26 17:14:59 +0700

    test

--- application/controllers/crontab/Report.php
+++ application/controllers/crontab/Report.php
@@ -177,6 +177,7 @@ class Report extends MY_Controller {
         while($page >= 1) {
             $a_conversion = $this->involve_asia_api->conversion($start_date, $end_date, NULL, $page, $limit);
             if(empty($a_conversion['data']['data'])) break;
+            echo $page . '==>';
             print_r($a_conversion['data']['data']);
             foreach($a_conversion['data']['data'] as $conversion) {
 
