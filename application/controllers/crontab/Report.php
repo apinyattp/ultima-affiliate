34149c8e9d4df7c33c35ecccfbc1faad1771670e
Apinya <apinya@harmonyx.co>
2021-01-29 17:57:17 +0700

    done admin

--- application/controllers/crontab/Report.php
+++ application/controllers/crontab/Report.php
@@ -8,12 +8,8 @@ class Report extends MY_Controller {
     }
 
     public function import() {
-        echo 11;
-        file_put_contents('./log_'.date("j.n.Y").'.log', '11111', FILE_APPEND);
         $this->accesstrade_conversion();
-        file_put_contents('./log_'.date("j.n.Y").'.log', '22222', FILE_APPEND);
         $this->admitad_conversion();
-        file_put_contents('./log_'.date("j.n.Y").'.log', '33333', FILE_APPEND);
         $this->involve_asia_conversion();
     }
 
