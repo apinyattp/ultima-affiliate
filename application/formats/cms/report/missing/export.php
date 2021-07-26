e0cc9dcb61b5d3503a5280cddf13370bf25c3b69
Apinya <apinya.ttp@gmail.com>
2021-07-26 15:08:19 +0700

    [edit][working] remove auth key & format dattime to date in missing report

--- application/formats/cms/report/missing/export.php
+++ application/formats/cms/report/missing/export.php
@@ -1,12 +1,10 @@
 <?php
 
 $this->load->model('campaign_model');
-$this->load->model('user_model');
 $a_campaign = $this->campaign_model->get_by_id($data['campaign_id']);
-$a_users = $this->user_model->check_by_uuid($data['uuid']);
 
 return [
-    'company' => empty($a_users) ? 'jelala' : $a_users['company'],
+    'company' => empty($data['company']) ? 'jelala' : $data['company'],
     'campaign_name' => $a_campaign['name'],
     'uid' => $data['uuid'],
     'amount' => $data['amount'],
--- application/formats/cms/report/missing/list.php
+++ application/formats/cms/report/missing/list.php
@@ -1,16 +1,14 @@
 <?php
 
 $this->load->model('campaign_model');
-$this->load->model('user_model');
 $a_campaign = $this->campaign_model->get_by_id($data['campaign_id']);
-$a_users = $this->user_model->check_by_uuid($data['uuid']);
 
 return [
     'id' => $data['id'],
     'campaign_id' => $data['campaign_id'],
     'a_campaign' => $a_campaign,
     'uid' => $data['uuid'],
-    'company' => empty($a_users) ? 'jelala' : $a_users['company'],
+    'company' => empty($data['company']) ? 'jelala' : $data['company'],
     'amount' => $data['amount'],
     'order_id' => $data['order_id'],
     'order_date' => date('Y-m-d', strtotime($data['order_date'])),
