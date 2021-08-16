e0805e90eb1fb336fb9da0bc22011efb3df15bc0
Apinya <apinya.ttp@gmail.com>
2021-08-16 18:55:04 +0700

    ff

--- application/modules/Admin/libraries/Authorization.php
+++ application/modules/Admin/libraries/Authorization.php
@@ -16,7 +16,6 @@ class Authorization {
 
         $this->_ci->load->helper('jwt');
         $payload = jwt_decode($token);
-
         // FAIL TO DECODE TOKEN
         if($payload === FALSE) return \E::INVALID_FORMAT_TOKEN;
 
@@ -26,7 +25,7 @@ class Authorization {
 
         // CHECK admin VAR
         if(empty($admin_id)) return \E::INVALID_FORMAT_TOKEN;
-        // if(empty($password_token)) return \E::INVALID_FORMAT_TOKEN;
+        if(empty($password_token)) return \E::INVALID_FORMAT_TOKEN;
         if(empty($expire)) return \E::INVALID_FORMAT_TOKEN;
 
         // CHECK EXPIRE
@@ -38,7 +37,7 @@ class Authorization {
         $a_admin = $this->_ci->admin_admin_model->get_by_id($admin_id);
 
         // CHECK admin DATA
-        // if($a_admin['password_token'] !== $password_token) return \E::PERMISSION_TOKEN_EXPIRE;
+        if($a_admin['password_token'] !== $password_token) return \E::PERMISSION_TOKEN_EXPIRE;
 
         // CHECK MODULE LOADED
         $admin_role = (bool) $this->_ci->load->find_module('admin_role');
