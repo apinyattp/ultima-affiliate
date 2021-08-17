007e54566463cc35d416aa6720a84e0c6fde7e7f
Apinya.T <apinya.ttp@gmail.com>
2021-08-17 16:16:17 +0700

    Update list.php

--- application/formats/cms/report/missing/list.php
+++ application/formats/cms/report/missing/list.php
@@ -14,5 +14,6 @@ return [
     'order_date' => date('Y-m-d H:i:s', strtotime($data['order_date'])),
     'status' => $data['status'],
     'source' => $data['source'],
+    'datetime_created' => $data['datetime_created'],
     'datetime_updated' => $data['datetime_updated']
 ];
