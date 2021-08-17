f711d898a969219bfbe68e706c3d98903696543a
Apinya.T <apinya.ttp@gmail.com>
2021-08-17 14:51:14 +0700

    Update search_filter.php

--- application/views/cms/missing_conversion/list/search_filter.php
+++ application/views/cms/missing_conversion/list/search_filter.php
@@ -1,16 +1,24 @@
 <form action="<?php echo site_url('cms/missing_conversion/list');?>"  method="get" autocomplete="off">
   <div class="row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-5">
       <label for="inputKeyword">Keyword (UID, Order ID)</label>
       <input id="keyword" name="keyword" type="text" class="form-control" placeholder="" value="<?php echo $keyword?>">
     </div>
-    <div class="form-group col-md-5">
+    <div class="form-group col-md-4">
         <label for="inputCampaign">Campaign</label>
         <select id="inputCampaign" id="campaign" name="campaign_id" class="form-control">
             <option value="" <?php if(empty($campaign_id)) echo 'selected' ?>>All</option>
             <?php foreach($a_campaign as $campaign) { ?>
                 <option value="<?php echo $campaign['id'] ?>" <?php if($campaign_id == $campaign['id']) echo 'selected' ?>><?php echo $campaign['name'] ?></option>
             <?php } ?>
+        </select>
+    </div>
+    <div class="form-group col-md-3">
+        <label for="inputCampaign">Source</label>
+        <select id="source" id="source" name="source" class="form-control">
+            <option value="" <?php if(empty($source)) echo 'selected' ?>>All</option>
+            <option value="accesstrade" <?php if($source == 'accesstrade') echo 'selected' ?>>Accesstrade</option>
+            <option value="involve_asia" <?php if($source == 'involve_asia') echo 'selected' ?>>Involve Asia</option>
         </select>
     </div>
   </div>
