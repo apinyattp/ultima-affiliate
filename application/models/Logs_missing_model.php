4f35dbbc8cd0696d27532e3141103311d09e82d7
Apinya.T <apinya.ttp@gmail.com>
2021-08-23 16:05:03 +0700

    Update Logs_missing_model.php

--- application/models/Logs_missing_model.php
+++ application/models/Logs_missing_model.php
@@ -7,4 +7,23 @@ class Logs_missing_model extends CI_Model {
         parent::__construct();
         $this->load->database();
     }
+    
+    public function insert_logs($id) {
+        $a_data = [
+            'report_coversion_id' => $id
+        ];
+        $this->db->insert('logs_missing', $a_data);
+        return TRUE;
+   }
+    
+    public function get_list($start_date=FALSE, $end_date=FALSE, $sort=FALSE) {
+        $this->load->library('qs');
+        if($start_date) $this->qs->where('datetime_updated >=',date('Y-m-d',strtotime($start_date)).' 00:00:00');
+        if($end_date) $this->qs->where('datetime_updated <=',date('Y-m-d',strtotime($end_date)).' 23:59:59');
+        if($sort) $this->qs->order_by($sort);
+        
+        return $this->qs->get('logs_missing');
+    }
+    
+   
 }
