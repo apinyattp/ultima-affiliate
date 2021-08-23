04dbcbd8b617427fd6762d6e83b06d749f0eeddc
Apinya.T <apinya.ttp@gmail.com>
2021-08-23 16:10:12 +0700

    Update Logs_missing_model.php

--- application/models/Logs_missing_model.php
+++ application/models/Logs_missing_model.php
@@ -10,7 +10,7 @@ class Logs_missing_model extends CI_Model {
     
     public function insert_logs($id) {
         $a_data = [
-            'report_conversion_id' => $id
+            'missing_id' => $missing_id
         ];
         $this->db->insert('logs_missing', $a_data);
         return TRUE;
