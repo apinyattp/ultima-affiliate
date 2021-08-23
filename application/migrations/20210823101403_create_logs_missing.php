060d09f3986368eff45fc63f1333074d6f88ffc8
Apinya.T <apinya.ttp@gmail.com>
2021-08-23 16:09:29 +0700

    Create 20210823101403_create_logs_missing.php

---
+++ application/migrations/20210823101403_create_logs_missing.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Create_logs_missing extends CI_Migration
+{
+	public function __construct()
+	{
+		$this->load->dbforge();
+		$this->load->database();
+	}
+
+	public function up()
+	{
+		$this->create_status_missing_table();
+	}
+
+	public function down()
+	{
+		$this->dbforge->drop_column('missing_conversion', 'status_reject');
+	}
+
+	private function create_status_missing_table()
+	{
+$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'INT',
+				'null' => FALSE,
+				'auto_increment' => TRUE,
+				'unsigned' => TRUE,
+				'constraint' => 11
+			),
+			'report_conversion_id' => array(
+				'type' => 'INT',
+				'null' => FALSE,
+				'unsigned' => TRUE,
+				'constraint' => 11
+				'comment' => 'report conversion id'
+			),
+      'missing_id' => array(
+				'type' => 'INT',
+				'null' => FALSE,
+				'unsigned' => TRUE,
+				'constraint' => 11
+				'comment' => 'missing id'
+			),
+		));
+    $this->dbforge->add_field('`datetime_updated` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->add_key(['missing_id', 'report_conversion_id']);
+		$this->dbforge->create_table('logs_missing', TRUE);
+	}
+}
+
+/* End of file 20210820101403_add_status_missing.php */
+/* Location: ./application/migrations/20210820101403_add_status_missing.php */
