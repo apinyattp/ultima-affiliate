86cfa627e7d0b379904a6610989de111e6170b5e
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-09-22 23:54:41 +0700

    add trip[ advisor

--- application/libraries/Involve_asia_api.php
+++ application/libraries/Involve_asia_api.php
@@ -63,7 +63,7 @@ class Involve_asia_api {
             'limit' => $limit,
             'sort_by' => 'relevant',
             'filters[country]' => 'Thailand',
-            'filters[offer_id]' => $offer_ids
+            'filters[offer_name]' => 'tripadvisor'
         ];
 
         $result = $this->_ci->gateway->curl_post($url, $params, $header);
