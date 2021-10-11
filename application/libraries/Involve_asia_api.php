5f1ca3299cb827689cf8c0e0991faf68f2240106
Apinya Mheaw <apinya.ttp@gmail.com>
2021-10-11 15:20:15 +0700

    Update Involve_asia_api.php

--- application/libraries/Involve_asia_api.php
+++ application/libraries/Involve_asia_api.php
@@ -94,6 +94,8 @@ class Involve_asia_api {
         ];
 
         $result = $this->_ci->gateway->curl_post($url, $params, $header);
+        print_r($result);
+        die();
         return json_decode($result, TRUE);
     }
 
