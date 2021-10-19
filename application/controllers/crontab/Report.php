918b09ed2d02cb60f8e6514bd1ae56dbb2bf7637
Apinya Mheaw <apinya.ttp@gmail.com>
2021-10-19 14:03:17 +0700

    Create Report.php

--- application/controllers/crontab/Report.php
+++ application/controllers/crontab/Report.php
@@ -185,12 +185,14 @@ class Report extends MY_Controller {
         $site_id = $this->config->item('tracking_link_id');
 
 
-        $start_date = date('Y-m-d', strtotime(date('Y-m-d').' -8 months'));
-        echo $start_date;die();
-        $end_date = date('Y-m-d');
-
+        //$start_date = date('Y-m-d', strtotime(date('Y-m-d').' -8 months'));
+        //$end_date = date('Y-m-d');
+        $start_date = date('Y-m-d', strtotime('2021-10-14'));
+        $end_date = date('Y-m-d', strtotime('2021-10-14'));
         while($page >= 1) {
             $a_conversion = $this->involve_asia_api->conversion($start_date, $end_date, NULL, $page, $limit);
+            print_r($a_conversion);
+            die();
             if(empty($a_conversion['data']['data'])) break;
 
             foreach($a_conversion['data']['data'] as $conversion) {
