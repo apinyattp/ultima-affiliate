48fd7422fe098a8889e3d537a68d81f0713cfabd
Nutdanai <nutdanai.tippa@gmail.com>
2021-12-05 16:22:21 +0700

    fix

--- application/formats/api/report/list.php
+++ application/formats/api/report/list.php
@@ -6,14 +6,12 @@ $a_campaign = $this->campaign_model->get_by_id($data['campaign_id']);
 
 $products = json_decode($data['products'], TRUE);
 
-if($data['source'] == 'accesstrade') {
-    foreach((array) $products as $key => $product) {
-        $category_reward = $this->campaign_model->get_category_reward_by_id($product['categoryId']);
-
-        $products[$key]['category'] = $this->format->run('api/report/campaign_category_reward', $category_reward);
-    }
-
-}
+// if($data['source'] == 'accesstrade') {
+//     foreach((array) $products as $key => $product) {
+//         $category_reward = $this->campaign_model->get_category_reward_by_id($product['categoryId']);
+//         $products[$key]['category'] = $this->format->run('api/report/campaign_category_reward', $category_reward);
+//     }
+// }
 
 return [
     'id' => $data['id'],
