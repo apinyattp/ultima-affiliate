0b7bae3e1f649afde6a439fd829ce58237bd71f0
Apinya Tanavongchinda <apinya.ttp@gmail.com>
2021-12-28 16:13:16 +0700

    add api secret

--- application/config/affiliate/safari.php
+++ application/config/affiliate/safari.php
@@ -4,6 +4,6 @@ $config = array(
     // 'endpoint' => 'https://adminsfw-uat.cfmanager.cloud/api/',
     'endpoint' => 'https://adminsfw.cfmanager.cloud/api/',
     'api_key' => 'cfmanager-safari',
-    'api_secret' => '16102020',
+    'api_secret' => '919302021',
     'main_id' => '2616008491'
 );
