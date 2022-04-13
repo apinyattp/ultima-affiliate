be6c6c30813b265685a39bf393be959b788cf027
Nutdanai <nutdanai.tippa@gmail.com>
2022-04-13 10:21:17 +0700

    [edit] cal usd to thb

--- application/controllers/Callback.php
+++ application/controllers/Callback.php
@@ -239,6 +239,13 @@ class Callback extends MY_Controller {
         $original_reward = $conversion['usd_payout'];
         $original_transaction_amount = $conversion['usd_sale_amount'];
 
+        switch ($currency) {
+            case 'USD':
+                $reward = $reward * 33;
+                $transaction_amount = $transaction_amount * 33;
+                break;
+        }
+
         $a_conversion = $this->report_conversion_model->get_by_conversion_id($conversion_id, $source);
         if($status != 'PENDING' && !empty($a_conversion)) {
             $this->report_conversion_model->update_status($a_conversion['id'], $status, $confirmation_time, $reward, $transaction_amount, $original_reward, $original_transaction_amount, $currency);
@@ -379,10 +386,10 @@ class Callback extends MY_Controller {
         $this->load->model('callback_model');
         $this->callback_model->create('nsq', $data);
     }
-    
+
     public function cfmanager() {
         $conversion = $this->input->post();
-        
+
         if(empty($conversion)) return $this->_echo_json(E::INVALID_FORMAT, ['error' => 'no data']);
 
         $this->load->model('callback_model');
@@ -400,7 +407,7 @@ class Callback extends MY_Controller {
 
     public function goship() {
         $conversion = $this->input->post();
-        
+
         if(empty($conversion)) return $this->_echo_json(E::INVALID_FORMAT, ['error' => 'no data']);
 
         $this->load->model('callback_model');
