0edc5b61a5ebca26a4cbf7786ad3d9c309ff151b
Nutdanai <nutdanai.tippa@gmail.com>
2022-05-09 09:53:00 +0700

    [edit] iship config

--- application/config/affiliate/iship.php
+++ application/config/affiliate/iship.php
@@ -1,8 +1,8 @@
 <?php
 
 $config = array(
-    'endpoint' => 'https://app-uat.iship.cloud/api/',
-    'api_key' => 'nKaG6IGaAjXM9WST13cI8IUj7cHkH6bt7RQJqMFo1r7qlAewRCpGT0v0wLKWRVqrPAqAVWzJvtMB2ouElBYZLMJwMqOs24F20E1f',
+    'endpoint' => 'https://app.weship.site/api/',
+    'api_key' => 'eyj49YXkOSTQgqzfPepbr7ooml8eWTbqwErR1pybuKjAOkPdohOEmGIILDHjL2iV2RAvlAtdXuo69FSu3bsxlK8duAzFTLSMUrqP',
     'main_id' => '99',
     'campaign_id' => '21110',
 );
