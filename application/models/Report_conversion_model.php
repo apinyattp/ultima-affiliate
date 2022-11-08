69ce47757bc8a2f48ef12c4f4da7803ec0165446
Nutdanai <nutdanai.tippa@gmail.com>
2022-11-08 17:49:32 +0700

    no message

--- application/models/Report_conversion_model.php
+++ application/models/Report_conversion_model.php
@@ -259,7 +259,7 @@ class Report_conversion_model extends CI_Model {
         if($status) $this->qs->where('status', $status);
         if($campaign_id) $this->qs->where('campaign_id', $campaign_id);
         if($keyword) {
-            $this->qs->like('verification_id', $keyword);
+            $this->qs->like('verification_id', '_'.$keyword, 'none');
             $sort = '';
             // $this->qs->group_start();
             //     $this->qs->where('uid', $keyword);
