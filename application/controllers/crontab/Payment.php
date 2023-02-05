c290daf53a65b3e381c92b636517e20953b8ba30
Nutdanai <nutdanai.tippa@gmail.com>
2023-02-05 21:36:13 +0700

    [fix] accesstrade paid status

--- application/controllers/crontab/Payment.php
+++ application/controllers/crontab/Payment.php
@@ -28,7 +28,7 @@ class Payment extends MY_Controller {
 
         foreach($a_payment as $payment) {
             var_dump($payment);
-            if($payment['status'] == 'UNPAID') break;
+            if($payment['paidAmount'] > 0) break;
 
             $fromDate = date('Y-m-01', strtotime($payment["paymentDate"]));
             $toDate = date('Y-m-t', strtotime($payment["paymentDate"]));
